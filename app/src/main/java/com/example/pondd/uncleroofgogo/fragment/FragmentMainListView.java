cd3b7fe0fc78711304f1015c8092f55cb198f3e6
ountzza <pond1982a@gmail.com>
2014-12-28 14:52:23 +0700

    basic view and fragment setup

---
+++ app/src/main/java/com/example/pondd/uncleroofgogo/fragment/FragmentMainListView.java
@@ -0,0 +1,54 @@
+package com.example.pondd.uncleroofgogo.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pondd.uncleroofgogo.R;
+
+/**
+ * Created by nuuneoi on 11/16/2014.
+ */
+public class FragmentMainListView extends Fragment {
+
+    public FragmentMainListView() {
+        super();
+    }
+
+    public static FragmentMainListView newInstance() {
+        FragmentMainListView fragment = new FragmentMainListView();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main_list_view, container, false);
+        initInstances(rootView);
+        return rootView;
+    }
+
+    private void initInstances(View rootView) {
+        // init instance with rootView.findViewById here
+        setRetainInstance(true);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+}
